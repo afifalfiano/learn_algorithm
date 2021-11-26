a1fa01174239e614c06c9c63f88a91ef62399004
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-11-26 15:46:54 +0700

    fix: init

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 
 function app(){}
 
-module.exports = app;
+module.exports = {app};
--- app.spec.js
+++ app.spec.js
@@ -1,6 +1,6 @@
 
 
-const app = require('./app.js');
+const {app} = require('./app.js');
 describe('Test Case', () => {
    it('...', () => {
 
