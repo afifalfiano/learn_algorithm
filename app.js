d0bce09678f310bac2a2438649a987794bd78543
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-11-28 17:38:55 +0700

    challenge-01

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 
-function app(){}
+function joinedLogger(level, sep) {
+  return function f(...messages) {
+    const data = [];
+      for(let i = 0; i < messages.length; i++) {
+          if (messages[i].level >= level) {
+                data.push(messages[i].text);
+          }
+      }
+    console.log(data.join(sep));
+    fs.createWriteStream(process.env.OUTPUT_PATH).write(data.join(sep));                
+  };
+}
+    
+function weekdayText(weekdays) {
+    return function getText(days)  {
+        if (days >= 0) {
+            for(let i = 0; i < weekdays.length; i++) {
+                if (days === i) {
+                    console.log(weekdays[i]);
+                    return weekdays[i];
+                }
+            }
+        }
+        throw "Error: Invalid weekday number";            
+    }
+}
 
-module.exports = {app};
+module.exports = {joinedLogger, weekdayText};
--- app.spec.js
+++ app.spec.js
@@ -1,6 +1,6 @@
 
 
-const {app} = require('./app.js');
+const {joinedLogger, weekdayText} = require('./app.js');
 describe('Test Case', () => {
    it('...', () => {
 
