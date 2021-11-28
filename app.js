a37eb9968e2ec69dc72a47fa0257c681c01d7b2b
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-11-28 09:07:12 +0700

    day-04: study case 1

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 
-function app(){}
+function gradingStudents(grades) {
+    // Write your code here
+    let roundedGrades = [];
+    const dataMultipleFive = [];
+    for(let j = 1; j <= 21; j++) {
+        dataMultipleFive.push(j * 5);
+    }
+    let roundedChange = [];
+    for (let k = 0; k < grades.length; k++) {
+        if (grades[k] < 38 && grades[k] >= 0) {
+            // not rounded
+            roundedGrades.push(grades[k]);
+        } 
+        for(let i = 0; i < dataMultipleFive.length; i++) {
+             if (grades[k] >= 38 && grades[k] <= 100) {
+                // rounded
+                const calculate = grades[k] - dataMultipleFive[i];
+                if (calculate > -3 && calculate < 0) {
+                    roundedGrades.push(grades[k] + (calculate * -1));
+                } else if (calculate > -5 && calculate <= -3) {
+                    roundedGrades.push(grades[k]);                    
+                } else if (calculate === 0){
+                    roundedGrades.push(grades[k]);                    
+                }
+            }
+        }
+    }
+    return roundedGrades;
+}
 
-module.exports = {app};
+module.exports = {gradingStudents};
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 
 #### Link Study Case
-(url)[url]
+(https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true)[https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true]
