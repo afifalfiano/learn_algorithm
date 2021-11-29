fff0fd6559987dea062b0a63286d17fda2e09e42
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-11-29 07:56:09 +0700

    day-05: study case 1

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 
-function app(){}
+function sockMerchant(n, ar) {
+    let pair = [];
+    if (n >= 1 && n <= 100) {
+        for(let i = 0; i < n; i++) {
+            for(let k = i + 1; k < n; k++) {
+                let calculate = ar[i] - ar[k];
+                if (calculate === 0) {
+                    console.log(ar[i], ar[k], calculate);
+                    pair.push(ar[i]);
+                    ar.splice(k, 1);
+                    break;
+                }
+            }
+        }
+    }
+    return pair.length;
 
-module.exports = {app};
+}
+module.exports = {sockMerchant};
--- app.spec.js
+++ app.spec.js
@@ -1,8 +1,9 @@
 
 
-const {app} = require('./app.js');
-describe('Test Case', () => {
-   it('...', () => {
-
+const {sockMerchant} = require('./app.js');
+describe('Pair Number', () => {
+   it('should return 3 pair when the input [10 20 20 10 10 30 50 10 20]', () => {
+      const data = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+      expect(sockMerchant(8, data)).toEqual(3);
    });
 });
