8099db376003da373464f142dbb6810899b2fc89
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-11-30 07:42:48 +0700

    day-06: study case 1

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 
-function app(){}
+function bonAppetit(bill, k, b) {
+    let bActual = 0;
+    let key = bill[k];
+    for(let i = 0; i < bill.length; i++) {
+        bActual += bill[i];
+    }
+    const splitBill = (bActual - key) / 2;
+    const bCharged = b - splitBill;
+    console.log(b, splitBill, bCharged, 'cek');
+    let result = 0;
+    bCharged === 0 ? result = "Bon Appetit" : result = bCharged;
+    console.log(result);
+    return result;
+}
 
-module.exports = {app};
+module.exports = {bonAppetit};
