205c49032333fb429313eed3c18c362f0d05d1e1
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-04-05 19:32:20 +0700

    day-07: continues learning

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 
 function app(){}
 
-module.exports = {app};
+function plusMinus(data){    
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] > 0) {
+            positive++;
+        } else if (data[i] < 0 ) {
+            negative-- ;
+        } else {
+            zero++;
+        }
+    }
+    
+    const resultPositive = positive / data.length;
+    const resultNegative = (negative / data.length) * -1;
+    const resultZero = zero / data.length;
+    // for(let i of dataFix) {
+    //     console.log(i);
+    // }
+    return [resultPositive.toFixed(6), resultNegative.toFixed(6), resultZero.toFixed(6)].join(', ');
+}
+
+module.exports = {app, plusMinus};
--- app.spec.js
+++ app.spec.js
@@ -1,8 +1,14 @@
 
 
-const {app} = require('./app.js');
+const {plusMinus} = require('./app.js');
 describe('Test Case', () => {
-   it('...', () => {
+   it('should return result 0.500000, 0.333333, 0.166667 when input data [-4, 3, -9, 0, 4, 1] ', () => {
+      const data =[-4, 3, -9, 0, 4, 1];
+      expect(plusMinus(data)).toEqual('0.500000, 0.333333, 0.166667')
+   });
 
+   it('should return result 0.400000, 0.400000, 0.200000 when input data [1,1,0,-1,-1] ', () => {
+      const data = [1,1,0,-1,-1];
+      expect(plusMinus(data)).toEqual('0.400000, 0.400000, 0.200000')
    });
 });
