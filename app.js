35c3051243dd54e3fa65d26d63a9e5bc600f0327
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-04-05 21:05:17 +0700

    day-07: add time conversion

--- app.js
+++ app.js
@@ -40,4 +40,19 @@ function minMaxSum(arr) {
 
 }
 
-module.exports = {app, plusMinus, minMaxSum};
+function timeConversion(s) {
+    let hours = s.substr(0, 2);
+    let minutes = s.substr(3, 2);
+    let seconds = s.substr(-4, 2);
+    let modifier = s.substr(-2, 2);
+    if (hours === '12') {
+        hours = '00';
+    }
+    if (modifier === 'PM') {
+        hours = parseInt(hours, 10) + 12;
+    }
+    let time = `${hours}:${minutes}:${seconds}`;
+    return time;
+}
+
+module.exports = {app, plusMinus, minMaxSum, timeConversion};
--- app.spec.js
+++ app.spec.js
@@ -1,6 +1,6 @@
 
 
-const {plusMinus, minMaxSum} = require('./app.js');
+const {plusMinus, minMaxSum, timeConversion} = require('./app.js');
 describe('Test Case', () => {
    it('should return result 0.500000, 0.333333, 0.166667 when input data [-4, 3, -9, 0, 4, 1] ', () => {
       const data =[-4, 3, -9, 0, 4, 1];
@@ -15,5 +15,13 @@ describe('Test Case', () => {
    it('should return 10 14 when value [1 2 3 4 5', () => {
       const data = [1,2,3,4,5];
       expect(minMaxSum(data)).toEqual('10 14');
+   })
+
+   it('should return 00:01:00 when input 12:01:00AM', () => {
+      expect(timeConversion('12:01:00AM')).toEqual('00:01:00');
+   })
+
+   it('should return 12:01:00 when input 12:01:00PM', () => {
+      expect(timeConversion('12:01:00PM')).toEqual('12:01:00');
    })
 });
